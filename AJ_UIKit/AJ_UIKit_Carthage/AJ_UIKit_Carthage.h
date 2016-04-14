4dbbdec5fd1d295866ec5c08b9020814a9977b71
钟宝健 <aboojaner@gmail.com>
2016-04-14 12:03:32 +0800

    支持 Carthage

---
+++ AJ_UIKit/AJ_UIKit_Carthage/AJ_UIKit_Carthage.h
@@ -0,0 +1,19 @@
+//
+//  AJ_UIKit_Carthage.h
+//  AJ_UIKit_Carthage
+//
+//  Created by 钟宝健 on 16/4/14.
+//  Copyright © 2016年 aboojan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AJ_UIKit_Carthage.
+FOUNDATION_EXPORT double AJ_UIKit_CarthageVersionNumber;
+
+//! Project version string for AJ_UIKit_Carthage.
+FOUNDATION_EXPORT const unsigned char AJ_UIKit_CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AJ_UIKit_Carthage/PublicHeader.h>
+
+
